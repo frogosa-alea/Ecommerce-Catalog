8554e4bc87b7f86f08a6ec565ca9738f8884d36d
frogosa-alea <benitezalea.f@gmail.com>
2022-08-01 04:21:15 +0800

    App routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Header from './containers/Header';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path='/' exact component={ProductListing}/>
+          <Route path='/product/:productId' exact component={ProductDetail}/>
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
